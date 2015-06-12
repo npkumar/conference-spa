73d60c4610f53ed4b6b9320d6d4e25f829d19ebd
Nitin Pradeep Kumar <nkumar@appcelerator.com>
2015-06-12 15:18:54 +0800

    set up routes for speaker

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./server/routes/index');
 var users = require('./server/routes/users');
+var speakers = require('./server/routes/speaker')
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/api/speakers', speakers);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
