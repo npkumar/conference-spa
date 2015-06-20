0ff03bb79afaf11b043ded29432f09bb624e4bf3
Nitin Pradeep Kumar <nkumar@appcelerator.com>
2015-06-20 13:06:58 +0800

    change routes as per arrow api

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var arrowUsers = require('../models/arrowUsers');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -8,55 +9,56 @@ router.get('/', function(req, res) {
 });
 
 /* GET profile page. */
-router.get('/profile', isLoggedIn, function(req, res) {
-	res.render('profile.ejs', {
-		user: req.user
-	});
-});
-
-// function to check if user is logged in
-function isLoggedIn(req, res, next) {
-	if (req.isAuthenticated()) {
-		return next();
+router.get('/profile', function(req, res) {
+	if (req.session.user) {
+		res.render('profile.ejs', {
+			user: req.session.user
+		});
+	} else {
+		res.redirect('/login');
 	}
-	// if not logged go to default route
-	res.redirect('/');
-}
+});
 
 /* GET logout route. */
 router.get('/logout', function(req, res) {
-	req.logout();
+	req.session = null;
 	res.redirect('/');
 });
 
 /* GET login page. */
 router.get('/login', function(req, res) {
 	res.render('login.ejs', {
-		message: req.flash('loginMessage')
+		message: []
 	});
 });
 
 /* POST login data. */
-router.post('/login', passport.authenticate('local-login', {
-	//Success go to Profile Page / Fail go to login page
-	successRedirect: '/profile',
-	failureRedirect: '/login',
-	failureFlash: true
-}));
+router.post('/login', function(req, res) {
+	arrowUsers.loginUser(req.body.email, req.body.password, function(session, user) {
+		console.log('Login: ' + session);
+		if (session) {
+			req.session.user = user;
+			res.redirect('/profile');
+		}
+	})
+});
 
 /* GET signup page. */
 router.get('/signup', function(req, res) {
 	res.render('signup.ejs', {
-		message: req.flash('signupMessage')
+		message: []
 	});
 });
 
 /* POST signup data. */
-router.post('/signup', passport.authenticate('local-signup', {
-	//Success go to Profile Page / Fail go to Signup page
-	successRedirect: '/profile',
-	failureRedirect: '/signup',
-	failureFlash: true
-}));
+router.post('/signup', function(req, res) {
+	arrowUsers.createUser(req.body.email, req.body.first_name, req.body.last_name, req.body.password, function(session) {
+		if (session) {
+			req.session = session;
+			res.redirect('/');
+		}
+	});
+
+});
 
 module.exports = router;
